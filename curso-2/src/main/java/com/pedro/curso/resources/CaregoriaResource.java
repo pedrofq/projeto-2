ebf31a63956af7fea6209349cdd0ae5def9ddd43
Pedro Quezado <pedro.fquezado@gmail.com>
2021-11-08 14:34:57 -0300

    REST mapping

---
+++ curso-2/src/main/java/com/pedro/curso/resources/CaregoriaResource.java
@@ -0,0 +1,15 @@
+package com.pedro.curso.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CaregoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST working!";
+	}
+}
