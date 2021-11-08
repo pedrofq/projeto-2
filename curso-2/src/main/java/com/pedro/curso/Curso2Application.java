d511e110a9c2e52d14640fd762b1a79d50c55b53
Pedro Quezado <pedro.fquezado@gmail.com>
2021-11-08 13:53:38 -0300

    V1

---
+++ curso-2/src/main/java/com/pedro/curso/Curso2Application.java
@@ -0,0 +1,13 @@
+package com.pedro.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso2Application.class, args);
+	}
+
+}
